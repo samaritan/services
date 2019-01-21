bcab9b4148da6d0f650bb7b91ce2e6102c13d9f7
Nuthan Munaiah <nuthan.munaiah@live.com>
2019-01-21 01:55:27 -0500

    Add service to collect Cyclomatic Complexity

---
+++ metrics/complexity/README.md
@@ -0,0 +1,3 @@
+# Complexity Metric Service
+
+> TODO
---
+++ metrics/complexity/complexity/service.py
@@ -0,0 +1,36 @@
+import logging
+import os
+
+from nameko.dependency_providers import Config
+from nameko.rpc import rpc, RpcProxy
+
+from .models import Complexity
+from .schemas import ComplexitySchema, MetricsSchema
+
+logger = logging.getLogger(__name__)
+METRICS = ['Cyclomatic']
+
+def _transform(metrics):
+    complexities = list()
+    for item in metrics:
+        entity = item.entity
+        complexity = item.metrics['Cyclomatic']
+        if entity.type == 'function' and complexity is not None:
+            complexities.append(Complexity(
+                entity=entity, complexity=complexity
+            ))
+    return complexities
+
+
+class ComplexityService:
+    name = 'complexity'
+
+    config = Config()
+    understand_rpc = RpcProxy('understand')
+
+    @rpc
+    def collect(self, project, processes=os.cpu_count(), **options):
+        logger.debug(project)
+        metrics = self.understand_rpc.get_metrics(project, METRICS)
+        metrics = MetricsSchema(many=True).load(metrics).data
+        return ComplexitySchema(many=True).dump(_transform(metrics)).data
