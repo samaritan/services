f2c6e4f6036a6bc9ca84760c4bc0a9d11049bd46
Nuthan Munaiah <nuthan.munaiah@live.com>
2019-07-22 16:56:03 -0400

    Add a YAML file containing git commands used
    The various git commands used by the service are hardcoded into the
    source code. The change introduces the ability to read the commands from
    an external YAML file.

---
+++ repository/repository/commands.py
@@ -0,0 +1,12 @@
+__all__ = ['COMMANDS']
+
+import logging
+import os
+
+from . import utilities
+
+logger = logging.getLogger(__name__)
+
+_PATH = os.path.abspath(os.path.dirname(__file__))
+_PATH = os.path.join(_PATH, 'commands.yml')
+COMMANDS = utilities.Yaml.read(_PATH)
