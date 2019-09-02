76411f3327a097566a2345d932a70c3b0f811415
Nuthan Munaiah <nuthan.munaiah@live.com>
2019-09-02 14:55:21 -0400

    Refactor keyword models and schemas

---
+++ metrics/keyword/keywrd/models/__init__.py
@@ -0,0 +1,4 @@
+from .keyword import Keyword
+from .project import Project
+from .repository import Change, Changes, Commit, Developer, File, Message,    \
+                        Module, Move, Moves, Patch
---
+++ metrics/keyword/keywrd/schemas/__init__.py
@@ -0,0 +1,5 @@
+from .keyword import KeywordSchema
+from .project import ProjectSchema
+from .repository import ChangeSchema, ChangesSchema, CommitSchema,            \
+                        DeveloperSchema, FileSchema, MessageSchema,           \
+                        ModuleSchema, MoveSchema, MovesSchema, PatchSchema
