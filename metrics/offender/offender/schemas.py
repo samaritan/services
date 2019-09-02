6f4e0a90c34c54ea7d32089f38322ee518d216f5
Nuthan Munaiah <nuthan.munaiah@live.com>
2019-09-02 15:14:50 -0400

    Refactor offender schema

--- metrics/offender/offender/schemas.py
+++ metrics/offender/offender/schemas.py
@@ -1,6 +1,12 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_load
+
+from .models import Offender
 
 
 class OffenderSchema(Schema):
     path = fields.String()
     cve = fields.String()
+
+    @post_load
+    def make_offender(self, data, **kwargs):
+        return Offender(**data)
