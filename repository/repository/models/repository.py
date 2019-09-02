51aafdb3dc336e965db7d56783db33d27a6ce41b
Nuthan Munaiah <nuthan.munaiah@live.com>
2019-09-02 14:02:26 -0400

    Remove Project class left behind from earlier

--- repository/repository/models/repository.py
+++ repository/repository/models/repository.py
@@ -83,18 +83,3 @@ class Patch:
 
     commit: Commit
     patch: str
-
-
-@dataclasses.dataclass(frozen=True)
-class Project:
-    __slots__ = [
-        'name', 'description', 'domain', 'language', 'project_url',
-        'repository_url'
-    ]
-
-    name: str
-    description: str
-    domain: str
-    language: str
-    project_url: str
-    repository_url: str
