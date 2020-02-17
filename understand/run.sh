328e8a81b81c9fe71b477abf6e59003a36962486
Nuthan Munaiah <nuthan.munaiah@live.com>
2020-02-17 13:30:51 -0500

    Set license code before running service
    The licensing service that Scitools uses seems to have be allowing the
    understand service to use the API without having to set a license.
    However, it appears that the apparent flaw in the licensing service
    seems to have been resolved which caused the understand service to fail
    with the NoApiLicense exception. The change sets up the license (stored
    in an environment variable) before starting the service.

--- understand/run.sh
+++ understand/run.sh
@@ -5,5 +5,7 @@ until nc -z ${RABBIT_HOST} ${RABBIT_PORT}; do
     sleep 3
 done
 
+und -setlicensecode "${UNDERSTAND_LICENSE:?UNDERSTAND_LICENSE has not been set}"
+
 # Run Service
 nameko run --config config.yml undrstand.service
