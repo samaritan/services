bcc57883f8b5569e7f187b695267490010ed1d62
Nuthan Munaiah <nuthan.munaiah@live.com>
2020-08-31 15:51:03 -0400

    Add contribution collection per commit

--- metrics/contribution/contribution/service.py
+++ metrics/contribution/contribution/service.py
@@ -1,12 +1,18 @@
 import logging
 
+from eventlet import GreenPool
 from nameko.dependency_providers import Config
 from nameko.rpc import rpc, RpcProxy
 
 from .contribution import get_contribution
-from .schemas import ChangesSchema, ContributionSchema
+from .schemas import ChangesSchema, CommitSchema, ContributionSchema
 
 logger = logging.getLogger(__name__)
+
+
+def _get_changes(project, commit, repository_rpc):
+    changes = repository_rpc.get_changes(project, commit.sha)
+    return ChangesSchema(many=True).load(changes)
 
 
 class ContributionService:
@@ -16,11 +22,17 @@ class ContributionService:
     repository_rpc = RpcProxy('repository')
 
     @rpc
-    def collect(self, project, **options):
+    def collect(self, project, sha=None, **options):
         logger.debug(project)
 
-        changes = self.repository_rpc.get_changes(project)
-        changes = ChangesSchema(many=True).load(changes)
+        commits = self.repository_rpc.get_commits(project, sha)
+        commits = CommitSchema(many=True).load(commits)
+
+        pool = GreenPool()
+        arguments = [(project, c, self.repository_rpc) for c in commits]
+        changes = list()
+        for item in pool.starmap(_get_changes, arguments):
+            changes.extend(item)
 
         contribution = get_contribution(changes, **options)
         return ContributionSchema(many=True).dump(contribution)
