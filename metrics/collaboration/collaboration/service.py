0eb63b137062025c1356f65136c5ed4a890da600
Nuthan Munaiah <nuthan.munaiah@live.com>
2020-08-31 15:40:29 -0400

    Add collaboration collection per commit

--- metrics/collaboration/collaboration/service.py
+++ metrics/collaboration/collaboration/service.py
@@ -1,12 +1,18 @@
 import logging
 
+from eventlet import GreenPool
 from nameko.dependency_providers import Config
 from nameko.rpc import rpc, RpcProxy
 
 from .collaboration import get_collaboration
-from .schemas import ChangesSchema, CollaborationSchema
+from .schemas import ChangesSchema, CollaborationSchema, CommitSchema
 
 logger = logging.getLogger(__name__)
+
+
+def _get_changes(project, commit, repository_rpc):
+    changes = repository_rpc.get_changes(project, commit.sha)
+    return ChangesSchema(many=True).load(changes)
 
 
 class CollaborationService:
@@ -16,11 +22,17 @@ class CollaborationService:
     repository_rpc = RpcProxy('repository')
 
     @rpc
-    def collect(self, project, **options):
+    def collect(self, project, sha=None, **options):
         logger.debug(project)
 
-        changes = self.repository_rpc.get_changes(project)
-        changes = ChangesSchema(many=True).load(changes)
+        commits = self.repository_rpc.get_commits(project, sha)
+        commits = CommitSchema(many=True).load(commits)
+
+        pool = GreenPool()
+        arguments = [(project, c, self.repository_rpc) for c in commits]
+        changes = list()
+        for item in pool.starmap(_get_changes, arguments):
+            changes.extend(item)
 
         collaboration = get_collaboration(changes, **options)
         return CollaborationSchema(many=True).dump(collaboration)
