9e63ffb9be9a9e45a5e40022fa55b91575a89f67
Nuthan Munaiah <nuthan.munaiah@live.com>
2020-08-31 17:08:12 -0400

    Add hunk collection per commit

--- metrics/hunk/hunk/hunk.py
+++ metrics/hunk/hunk/hunk.py
@@ -8,11 +8,6 @@ logger = logging.getLogger(__name__)
 HUNKHEADER_RE = re.compile(r'(?:\n@@)')
 
 
-def get_hunks(patches):
-    hunks = list()
-
-    for patch in patches:
-        hunk = len(HUNKHEADER_RE.findall(patch.patch))
-        hunks.append(Hunk(commit=patch.commit, hunk=hunk))
-
-    return hunks
+def get_hunk(patch):
+    hunk = len(HUNKHEADER_RE.findall(patch.patch))
+    return Hunk(commit=patch.commit, hunk=hunk)
--- metrics/hunk/hunk/service.py
+++ metrics/hunk/hunk/service.py
@@ -1,13 +1,19 @@
 import logging
 
+from eventlet import GreenPool
 from nameko.dependency_providers import Config
 from nameko.rpc import rpc, RpcProxy
 
-from . import utilities
-from .hunk import get_hunks
-from .schemas import HunkSchema, PatchSchema
+from .hunk import get_hunk
+from .schemas import CommitSchema, HunkSchema, PatchSchema
 
 logger = logging.getLogger(__name__)
+
+
+def _get_hunks(project, commit, repository_rpc):
+    patch = repository_rpc.get_patch(project, commit.sha)
+    patch = PatchSchema().load(patch)
+    return get_hunk(patch)
 
 
 class HunkService:
@@ -17,15 +23,20 @@ class HunkService:
     repository_rpc = RpcProxy('repository')
 
     @rpc
-    def collect(self, project, **options):
+    def collect(self, project, sha=None, **options):
         logger.debug(project)
 
+        commits = None
+        if sha is None:
+            commits = self.repository_rpc.get_commits(project)
+        else:
+            commits = [self.repository_rpc.get_commit(project, sha)]
+        commits = CommitSchema(many=True).load(commits)
+
+        pool = GreenPool()
+        arguments = ((project, c, self.repository_rpc) for c in commits)
         hunks = list()
-        commits = self.repository_rpc.get_commits(project)
-        for chunk in utilities.chunk(commits, size=round(len(commits) * 0.05)):
-            patches = self.repository_rpc.get_patches(project, chunk)
-            patches = PatchSchema(many=True).load(patches)
-            hunks.extend(get_hunks(patches))
-            patches.clear()
+        for item in pool.starmap(_get_hunks, arguments):
+            hunks.append(item)
 
         return HunkSchema(many=True).dump(hunks)
