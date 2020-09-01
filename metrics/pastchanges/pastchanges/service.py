2b301335a7bfa55b1faed8dbf8c56d479b36aebd
Nuthan Munaiah <nuthan.munaiah@live.com>
2020-08-31 23:29:24 -0400

    Add past changes collection per commit

--- metrics/pastchanges/pastchanges/service.py
+++ metrics/pastchanges/pastchanges/service.py
@@ -1,19 +1,24 @@
 import logging
 
+from eventlet import GreenPool
 from nameko.dependency_providers import Config
 from nameko.rpc import rpc, RpcProxy
 
 from .models import PastChanges
-from .schemas import ChangesSchema, PastChangesSchema
+from .schemas import ChangesSchema, CommitSchema, PastChangesSchema
 
 logger = logging.getLogger(__name__)
+
+
+def _get_changes(project, commit, repository_rpc):
+    changes = repository_rpc.get_changes(project, commit.sha)
+    return ChangesSchema(many=True).load(changes)
 
 
 def _get_pastchanges(changes):
     pastchanges = list()
 
-    # Assume changes from repository service are in reverse chronological order
-    changes.reverse()
+    changes = sorted(changes, key=lambda i: i.commit.timestamp)
 
     paths = dict()
     for change in changes:
@@ -35,11 +40,19 @@ class PastChangesService:
     repository_rpc = RpcProxy('repository')
 
     @rpc
-    def collect(self, project, **options):
+    def collect(self, project, sha=None, **options):
         logger.debug(project)
 
-        changes = self.repository_rpc.get_changes(project)
-        changes = ChangesSchema(many=True).load(changes)
+        commits = self.repository_rpc.get_commits(project, sha)
+        commits = CommitSchema(many=True).load(commits)
+
+        pool = GreenPool()
+        arguments = [(project, c, self.repository_rpc) for c in commits]
+        changes = list()
+        for item in pool.starmap(_get_changes, arguments):
+            changes.extend(item)
         pastchanges = _get_pastchanges(changes)
+        if sha is not None:
+            pastchanges = [i for i in pastchanges if i.commit.sha == sha]
 
         return PastChangesSchema(many=True).dump(pastchanges)
