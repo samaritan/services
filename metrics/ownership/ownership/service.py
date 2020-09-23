b1b112b6a2c7eecf7ad51c635597d4ec1d9c3a40
Nuthan Munaiah <nuthan.munaiah@live.com>
2020-09-22 23:29:40 -0400

    Add ownership collection for developer in commit

--- metrics/ownership/ownership/service.py
+++ metrics/ownership/ownership/service.py
@@ -9,6 +9,18 @@ from .schemas import CommitSchema, OwnershipSchema
 logger = logging.getLogger(__name__)
 
 
+def _get_name_filter(name):
+    def _filter(value):
+        return value.developer.name == name
+    return _filter
+
+
+def _get_email_filter(email):
+    def _filter(value):
+        return value.developer.email == email
+    return _filter
+
+
 class OwnershipService:
     name = 'ownership'
 
@@ -16,10 +28,16 @@ class OwnershipService:
     repository_rpc = RpcProxy('repository')
 
     @rpc
-    def collect(self, project, sha, **options):
+    def collect(self, project, sha, name=None, email=None, **options):
         logger.debug(project)
 
         commits = self.repository_rpc.get_commits(project, sha)
         commits = CommitSchema(many=True).load(commits)
         ownerships = get_ownership(commits)
+
+        if name is not None:
+            ownerships = filter(_get_name_filter(name), ownerships)
+        if email is not None:
+            ownerships = filter(_get_email_filter(email), ownerships)
+
         return OwnershipSchema(many=True).dump(ownerships)
