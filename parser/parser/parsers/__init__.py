7385fc8a309c7c59c902aca726b9570a7729fbdd
Nuthan Munaiah <nuthan.munaiah@live.com>
2020-10-07 00:02:44 -0400

    Use SrcMLParser to parse C and C++ source code
    The change is in service of #18.

--- parser/parser/parsers/__init__.py
+++ parser/parser/parsers/__init__.py
@@ -1,10 +1,10 @@
-from .cparser import CParser
+from .srcmlparser import SrcMLParser
 from ..exceptions import NoParser
 
-PARSERS = {'C': CParser, 'C++': CParser}
+PARSERS = {'C': SrcMLParser, 'C++': SrcMLParser}
 
 
 def get_parser(language):
     if language not in PARSERS:
         raise NoParser(f'No parser to parse files written in {language}')
-    return PARSERS[language]()
+    return PARSERS[language](language)
