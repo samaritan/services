3db34ea39a865fe15a6808e883b29b5676d7fe02
Nuthan Munaiah <nuthan.munaiah@live.com>
2020-10-06 23:58:52 -0400

    Use language names consistent with GitHub Linguist
    The `languages.yml` file of the `parser` service is similar in purpose
    to GitHub Linguist `languages.yml` [1] file. However, the casing of the
    name of the programming languages is different. The change corrects the
    casing of the programming language to be consistent with GitHub
    Linguist.
    
    References
    
    [1] https://github.com/github/linguist/blob/master/lib/linguist/languages.yml

--- parser/parser/parsers/__init__.py
+++ parser/parser/parsers/__init__.py
@@ -1,11 +1,10 @@
 from .cparser import CParser
 from ..exceptions import NoParser
 
-PARSERS = {'c': CParser, 'c++': CParser}
+PARSERS = {'C': CParser, 'C++': CParser}
 
 
 def get_parser(language):
-    language = language.lower()
     if language not in PARSERS:
         raise NoParser(f'No parser to parse files written in {language}')
     return PARSERS[language]()
