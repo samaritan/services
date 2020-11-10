c6bd510e7bc6f6b0abef3ef5939e67dc6248b459
Nuthan Munaiah <nuthan.munaiah@live.com>
2020-11-09 22:05:02 -0500

    Update project model and schema
    The removal of `description`, `domain`, and `project_url and the
    addition of `owner` fields to the `Project` instance in `project`
    service necessitated the change. See 0381c49 for the change to
    `project.Project`.

--- metrics/functionchurn/functionchurn/models/project.py
+++ metrics/functionchurn/functionchurn/models/project.py
@@ -3,15 +3,10 @@ import dataclasses
 
 @dataclasses.dataclass(frozen=True)
 class Project:
-    __slots__ = [
-        'id', 'name', 'description', 'domain', 'language', 'project_url',
-        'repository_url'
-    ]
+    __slots__ = ['id', 'owner', 'name', 'language', 'repository_url']
 
     id: int
+    owner: str
     name: str
-    description: str
-    domain: str
     language: str
-    project_url: str
     repository_url: str
--- metrics/functionchurn/functionchurn/schemas/project.py
+++ metrics/functionchurn/functionchurn/schemas/project.py
@@ -2,13 +2,12 @@ from marshmallow import Schema, fields, post_load
 
 from ..models import Project
 
+
 class ProjectSchema(Schema):
     id = fields.Integer()
+    owner = fields.String()
     name = fields.String()
-    description = fields.String()
-    domain = fields.String()
     language = fields.String()
-    project_url = fields.String()
     repository_url = fields.String()
 
     @post_load
