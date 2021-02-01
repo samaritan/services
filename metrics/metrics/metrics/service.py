f3a69029da97764046e023ac2b4d2d189625023c
Nuthan Munaiah <nuthan.munaiah@live.com>
2021-02-01 14:58:04 -0500

    Associate metrics on `project_created`

--- metrics/metrics/metrics/service.py
+++ metrics/metrics/metrics/service.py
@@ -1,6 +1,6 @@
 import logging
 
-from nameko.events import EventDispatcher
+from nameko.events import EventDispatcher, event_handler
 from nameko.dependency_providers import Config
 from nameko.rpc import rpc
 from nameko_sqlalchemy import DatabaseSession
@@ -28,3 +28,16 @@ class MetricsService:
                                .filter_by(enabled=True) \
                                .all()
         return MetricSchema(many=True).dump(metrics)
+
+    @event_handler('project', 'project_created')
+    def handle_project_created(self, payload):
+        logger.debug('handle_project_created')
+        project = ProjectSchema().load(payload.get('project'))
+        metrics = self.database.query(Metric.id, Metric.enabled)
+        for metric in metrics:
+            project_metric = ProjectMetric(
+                project_id=project.id, metric_id=metric.id,
+                enabled=metric.enabled
+            )
+            self.database.add(project_metric)
+        self.database.commit()
